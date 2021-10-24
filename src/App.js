063a6ed1da226868baea9ac2c2dd3c2e2eaf1c34
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-10-24 13:23:53 +0600

    POST

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import axios from 'axios';
 
 class App extends Component {
   componentDidMount() {
-    axios.get('http://localhost:5000/dishes')
-      .then(response => response.data)
-      .then(data => console.log(data))
-      .catch(error => console.log(error.message));
+    // axios.get('http://localhost:5000/dishes')
+    //   .then(response => response.data)
+    //   .then(data => console.log(data))
+    //   .catch(error => console.log(error.message));
+
+    axios.post('http://localhost:5000/dishes', { name: "Pizza", price: "799" })
+      .then(response => console.log(response))
+
   }
   render() {
     return (
