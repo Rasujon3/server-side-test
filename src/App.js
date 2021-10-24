47d34eb8491668ba93fcb6470c6b38334d7b3315
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-10-24 13:02:56 +0600

    use fatch to api call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import fetch from 'cross-fetch';
 import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
   render() {
     return (
       <div>
