876d2c89e77d5732f8f8b9433fafe7c14b65b6ac
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-10-24 13:37:57 +0600

    Handling response using state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import './App.css';
 import axios from 'axios';
 
 class App extends Component {
+  state = {
+    dishes: null,
+    errMess: null
+  }
   componentDidMount() {
+    // console.log("ComponentDidMount: ", this.state);
     // axios.get('http://localhost:5000/dishes')
     //   .then(response => response.data)
-    //   .then(data => console.log(data))
-    //   .catch(error => console.log(error.message));
+    //   .then(data => {
+    //     this.setState({
+    //       dishes: data
+    //     })
+    //   })
+    //   .catch(error => {
+    //     this.setState({
+    //       errMess: error.message
+    //     })
+    //   });
 
     // axios.post('http://localhost:5000/dishes', { name: "Pizza", price: "799" })
     //   .then(response => console.log(response))
@@ -17,7 +30,15 @@ class App extends Component {
 
     axios.delete('http://localhost:5000/dishes/0')
       .then(response => console.log(response))
+      .catch(error => {
+        this.setState({
+          errMess: error.message
+        })
+      });
 
+  }
+  componentDidUpdate() {
+    console.log("Update: ", this.state);
   }
   render() {
     return (
