0741263c43a41d2f0c3ccc686eca4ef8383cc0a8
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-10-24 13:10:47 +0600

    Axios use for api handle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import fetch from 'cross-fetch';
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(data => console.log(data));
+    // fetch('https://jsonplaceholder.typicode.com/posts')
+    //   .then(response => response.json())
+    //   .then(data => console.log(data));
+
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.data)
+      .then(data => console.log(data))
   }
   render() {
     return (
